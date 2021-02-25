835834c6fede4f01f4509b7b1e16be2f9de3f0ff
Sashin-Amichand <sashin.amichand@my.bdsc.school.nz>
2021-02-25 14:38:33 +1300

    3rd Intro Done

--- password_manager.py
+++ password_manager.py
@@ -1,24 +1,44 @@
 #password_manager.py
 #create and display password for the user
 #S.Amichand, Feb 22
-import sys
+import sys 
 
-# Informs the user about the program
-print("Hello there! This program is designed for you to store your passwords and users.\
-This is only for people who are 13 or older.")
+# Variables and Lists I will use
+name = ""
+age = ""
+user = ["sashin",]
+password = ["Helloyou123", ]
 
 # Asks the user their age and name
 def question():
     while True:
+        global name
         name = input("What is your name?\n")
-        age = float(input("How old are you?\n"))
-        if age > 13:
-            print("Sorry you are too young.")
-            sys.exit()
+        if name == "":
+            print("Please type something")
         else:
-            print("You are old enough {}" .format(name))
-            global login
-            login = input("{} Do you have an account or do you wish to create one? If you wish to create one type 1, if you wish to log in an existing one, type 2:\n"))
-
-
+            break
+    while True:      
+        try:
+            global age
+            age = float(input("How old are you?\n").strip())
+            if age < 13:
+                print("Sorry you are too young.")
+                break
+            else:
+                print("You are old enough {}." .format(name))
+                signingIn()
+        except ValueError: 
+            print("Please only type a number")
+          
+# LOGGING IN 
 # Asks if the user wishes to log in or create an account
+def signingIn():
+    print("Hi")
+    sys.exit()
+    
+# INTRODUCTION
+# Informs the user about the program
+print("Hello there! This program is designed for you to store your passwords and users.\
+This is only for people who are 13 or older.")
+question()
